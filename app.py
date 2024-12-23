3a9af7406fdbee094c6a125ecddcfec953c7f6fa
RyanWu9863 <ryan061300@gmail.com>
2024-12-24 01:12:35 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def callback():
 def handle_message(event):
     user_message = event.message.text
     if "天氣" in user_message:
-        response = get_weather("台灣")
+        response = get_weather()
     elif "新聞" in user_message:
         response = get_news()
     else:
