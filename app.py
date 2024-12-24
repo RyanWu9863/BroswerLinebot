641d9a0df07f9f8fb2de1618254bfa7028538257
RyanWu9863 <ryan061300@gmail.com>
2024-12-24 15:42:42 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from weather import get_weather
-from news import get_news
+import json,requests
+import requests
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
@@ -32,53 +32,58 @@ def callback():
 
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
+cities = ['基隆市','嘉義市','臺北市','嘉義縣','新北市','臺南市','桃園縣','高雄市','新竹市','屏東縣','新竹縣','臺東縣','苗栗縣','花蓮縣','臺中市','宜蘭縣','彰化縣','澎湖縣','南投縣','金門縣','雲林縣','連江縣']
+
+def get_weather(city):
+    api_key = "CWA-3995D289-1BE9-45B6-AD6F-5496915DB347"
+    url =f'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization={api_key}&format=JSON&locationName={city}'
+    Data = requests.get(url)
+    Data = (json.loads(Data.text,encoding="utf-8"))['records']['location'][0]['weatherElement']  
+    res = [[], [], []]
+    for j in range(3):
+        for i in Data:
+            res[j].append(i["time"][j])
+    return res
+
+@handler.add(MessageEvent)
 def handle_message(event):
     user_message = event.message.text
-    if "天氣" in user_message:
-        response = get_weather("Taipei")
-    elif "新聞" in user_message:
-        response = get_news()
+    if (user_message[:2] == "天氣"):
+        city = user_message[3:]
+        city = city.replace("台", "臺")
+        if (not (city in cities)):
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="查詢格式為: 天氣 縣市")) 
+        else:
+            res = get_weather(city)
+            line_bot_api.reply_message(event.reply_token, TemplateSendMessage(
+                alt_text= city + "未來 36 小時天氣預測",
+                template = CarouselTemplate(
+                    columns = [
+                        CarouselColumn(
+                            thumbnail_image_url = "",
+                            title = '{} ~ {}'.format(res[0][0]['startTime'][5:-3],res[0][0]['endTime'][5:-3]),
+                            text = '天氣狀況 {}\n溫度 {} ~ {} °C\n降雨機率 {}'.format(data[0]['parameter']['parameterName'],data[2]['parameter']['parameterName'],data[4]['parameter']['parameterName'],
+                            data[1]['parameter']['parameterName']),
+                            actions = [
+                                URIAction(
+                                    label = '詳細內容',
+                                    uri = 'https://www.cwb.gov.tw/V8/C/W/County/index.html'
+                                )
+                            ]
+                        )for data in res           
+                    ]
+                )
+            ))
     else:
-        response = "請輸入「天氣」或「新聞」來獲取資訊。"
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=response))
+        line_bot_api.reply_message(reply_token, TextSendMessage(text=message))
 
-# def get_taiwan_weather(location):
-#     api_key = "CWA-3995D289-1BE9-45B6-AD6F-5496915DB347"
-#     url = f"https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization={api_key}&locationName={location}"
-#     try:
-#         response = requests.get(url, timeout=10)
-#         response.raise_for_status()
-#         data = response.json()
-#         location_data = data['records']['location'][0]
-#         weather = location_data['weatherElement'][0]['time'][0]['parameter']['parameterName']
-#         temperature = location_data['weatherElement'][2]['time'][0]['parameter']['parameterName']
-#         return f"{location} 天氣：{weather}，溫度：{temperature}°C"
-#     except requests.exceptions.RequestException:
-#         return "無法取得天氣資訊，請稍後再試。"
-#     except IndexError:
-#         return "查無此地區天氣資訊，請確認地名是否正確。"
 
-# # 必須放上自己的 NewsAPI API Key
-# NEWS_API_KEY = "73f4a1974f104ae3b322e7e356ad6f9d"
 
-# def fetch_taiwan_news(keyword):
-#     url = f"https://newsapi.org/v2/top-headlines?country=tw&category={news_category}&apiKey={NEWS_API_KEY}"
-
-#     try:
-#         response = requests.get(url, timeout=10)
-#         response.raise_for_status()
-#         data = response.json()
-
-#         if data["status"] != "ok" or not data["articles"]:
-#             return "查無相關新聞，請嘗試其他關鍵字。"
-
-#         # 組合新聞資訊
-#         articles = data["articles"]
-#         return "\n".join(f"{article['title']} - {article['url']}" for article in articles)
-#     except requests.exceptions.RequestException:
-#         return "無法取得新聞資訊，請稍後再試。"
-
+# if "新聞" in user_message:
+#         response = get_news()
+#     else:
+#         response = "請輸入「天氣」或「新聞」來獲取資訊。"
+#     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=response))
 
 #主程式
 import os
