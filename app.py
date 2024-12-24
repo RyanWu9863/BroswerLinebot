6aa9c79a15e22054118a87cab869b94e9ff32be1
RyanWu9863 <ryan061300@gmail.com>
2024-12-24 23:37:56 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def handle_message(event):
                 template = CarouselTemplate(
                     columns = [
                         CarouselColumn(
-                            thumbnail_image_url = "https://i.imgur.com/Ex3Opfo.png",
+                            thumbnail_image_url = "",
                             title = '{} ~ {}'.format(res[0][0]['startTime'][5:-3],res[0][0]['endTime'][5:-3]),
                             text = '天氣狀況 {}\n溫度 {} ~ {} °C\n降雨機率 {}'.format(data[0]['parameter']['parameterName'],data[2]['parameter']['parameterName'],data[4]['parameter']['parameterName'],
                             data[1]['parameter']['parameterName']),
