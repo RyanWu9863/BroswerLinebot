8a40915d1b9e04c68990b9b0cc9df05840871862
RyanWu9863 <ryan061300@gmail.com>
2024-12-29 00:04:20 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-#載入LineBot所需要的套件
+# -*- coding: utf-8 -*-
+
+# 載入 LineBot 所需要的套件
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import json,requests
-import requests
+from linebot.models import *
+import re
+import os  # 確保匯入 os 模組
+
 app = Flask(__name__)
 
-# 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('S7DP6ph0sWeOWVyIr9c7ZV3NuqYkVlTfY5+CSnWblopXBqRFYuU8HSaaAQ9nNWYo3Ufdm/q6OxemxpP7wqFw5XwXkFvlwaf+pwKdp5BlgewaeaVILO3FUi5xbISRjNyhSzMGxIfVEn6HWYVeOiyzqQdB04t89/1O/w1cDnyilFU=')
-# 必須放上自己的Channel Secret
+# 必須放上自己的 Channel Access Token
+line_bot_api = LineBotApi('66YmqXw11hVa6I7CBtb3gtWoTvhnhbXul0z6sX71D/5XQhOALiLzoDvo3wz7i0+15IXcV/LNdzLcBesfJN+71QSk8CFUfc8GnsZ9wmxdwxJ+jdNtNs7vneYrzoin9AfJ3gv6djQU5Hu9B7/g04bTRgdB04t89/1O/w1cDnyilFU=S7DP6ph0sWeOWVyIr9c7ZV3NuqYkVlTfY5+CSnWblopXBqRFYuU8HSaaAQ9nNWYo3Ufdm/q6OxemxpP7wqFw5XwXkFvlwaf+pwKdp5BlgewaeaVILO3FUi5xbISRjNyhSzMGxIfVEn6HWYVeOiyzqQdB04t89/1O/w1cDnyilFU=')
+
+# 必須放上自己的 Channel Secret
 handler = WebhookHandler('65be4975efb12e6e52a9ef33e73f393b')
 
 line_bot_api.push_message('U99de7fa38147448fa75424b52482549f', TextSendMessage(text='你可以開始了'))
@@ -19,74 +23,142 @@ line_bot_api.push_message('U99de7fa38147448fa75424b52482549f', TextSendMessage(t
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
-cities = ['基隆市','嘉義市','臺北市','嘉義縣','新北市','臺南市','桃園市','高雄市','新竹市','屏東縣','新竹縣','臺東縣','苗栗縣','花蓮縣','臺中市','宜蘭縣','彰化縣','澎湖縣','南投縣','金門縣','雲林縣','連江縣']
-
-def get_weather(city):
-    api_key = "CWA-3995D289-1BE9-45B6-AD6F-5496915DB347"
-    url =f'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization={api_key}&format=JSON&locationName={city}'
-    Data = requests.get(url)
-    Data = (json.loads(Data.text,encoding="utf-8"))['records']['location'][0]['weatherElement']  
-    res = [[], [], []]
-    for j in range(3):
-        for i in Data:
-            res[j].append(i["time"][j])
-    return res
-
-@handler.add(MessageEvent)
+# 訊息傳遞區塊
+##### 基本上程式編輯都在這個 function #####
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     user_message = event.message.text
-    if (user_message[:2] == "天氣"):
-        city = user_message[3:]
-        city = city.replace("台", "臺")
-        if (not (city in cities)):
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="查詢格式為: 天氣 縣市")) 
-        else:
-            res = get_weather(city)
-            line_bot_api.reply_message(event.reply_token, TemplateSendMessage(
-                alt_text= city + "未來 36 小時天氣預測",
-                template = CarouselTemplate(
-                    columns = [
-                        CarouselColumn(
-                            thumbnail_image_url = "",
-                            title = '{} ~ {}'.format(res[0][0]['startTime'][5:-3],res[0][0]['endTime'][5:-3]),
-                            text = '天氣狀況 {}\n溫度 {} ~ {} °C\n降雨機率 {}'.format(data[0]['parameter']['parameterName'],data[2]['parameter']['parameterName'],data[4]['parameter']['parameterName'],
-                            data[1]['parameter']['parameterName']),
-                            actions = [
-                                URIAction(
-                                    label = '詳細內容',
-                                    uri = 'https://www.cwb.gov.tw/V8/C/W/County/index.html'
-                                )
-                            ]
-                        )for data in res           
-                    ]
-                )
-            ))
+
+    # 功能：旅遊景點推薦
+    if re.match('我想出去玩', user_message):
+        carousel_template_message = TemplateSendMessage(
+            alt_text='熱門旅行景點',
+            template=CarouselTemplate(
+                columns=[
+                    CarouselColumn(
+                        thumbnail_image_url='https://i.imgur.com/Ay7IkdS.jpg',
+                        title='北部推薦景點',
+                        text='North',
+                        actions=[
+                            MessageAction(
+                                label='導覽',
+                                text='台灣北部擁有多元景點，如九份、陽明山、淡水等。'
+                            ),
+                            URIAction(
+                                label='詳細資訊',
+                                uri='https://taiwantour.net/taiwan-attractions/'
+                            )
+                        ]
+                    ),
+                    CarouselColumn(
+                        thumbnail_image_url='https://i.imgur.com/7NRdD4E.jpg',
+                        title='中部推薦景點',
+                        text='West',
+                        actions=[
+                            MessageAction(
+                                label='導覽',
+                                text='台灣中部有清境農場、日月潭等迷人景點。'
+                            ),
+                            URIAction(
+                                label='詳細資訊',
+                                uri='https://travel.line.me/article/A1fp6dm8v0'
+                            )
+                        ]
+                    )
+                    # 可繼續加入其他地區的推薦
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token, carousel_template_message)
+
+    # 功能：電影推薦
+    elif re.match('推薦電影', user_message):
+        image_carousel_template_message = TemplateSendMessage(
+            alt_text='推薦電影清單',
+            template=ImageCarouselTemplate(
+                columns=[
+                    ImageCarouselColumn(
+                        image_url='https://i.imgur.com/miOnNkN.jpg',
+                        action=PostbackAction(
+                            label='SPY×FAMILY',
+                            display_text=(
+                                "劇情介紹: 北國旅遊溫馨故事。\n"
+                                "上映日期: 2024/1/19\n"
+                                "觀看預告片: https://www.youtube.com/watch?v=-4g5k-WoT-g"
+                            ),
+                            data='action=SPY_FAMILY'
+                        )
+                    ),
+                    ImageCarouselColumn(
+                        image_url='https://i.imgur.com/UKtun5m.jpg',
+                        action=PostbackAction(
+                            label='排球少年',
+                            display_text=(
+                                "劇情介紹: 烏野高中與音駒高中的宿命對決。\n"
+                                "上映日期: 2024/4/12\n"
+                                "觀看預告片: https://www.youtube.com/watch?v=gK6RbuM3U7Y"
+                            ),
+                            data='action=HAIKYUU'
+                        )
+                    )
+                    # 可繼續加入其他電影推薦
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token, image_carousel_template_message)
+
+    # 功能：告訴我秘密（圖片訊息）
+    elif re.match('告訴我秘密', user_message):
+        image_message = ImageSendMessage(
+            original_content_url='https://www.campus-studio.com/download/flag.jpg',
+            preview_image_url='https://www.campus-studio.com/download/101.jpg'
+        )
+        line_bot_api.reply_message(event.reply_token, image_message)
+
+    # 功能：附近的各樣式餐廳（Imagemap 訊息）
+    elif re.match('附近的各樣式餐廳', user_message):
+        imagemap_message = ImagemapSendMessage(
+            base_url='https://i.imgur.com/ZLFh4RV.png',
+            alt_text='推薦餐廳',
+            base_size=BaseSize(height=1040, width=1040),
+            actions=[
+                URIImagemapAction(
+                    link_uri='https://www.google.com/maps?q=日式料理',
+                    area=ImagemapArea(x=0, y=0, width=520, height=520)
+                ),
+                URIImagemapAction(
+                    link_uri='https://www.google.com/maps?q=西式料理',
+                    area=ImagemapArea(x=520, y=0, width=520, height=520)
+                ),
+                URIImagemapAction(
+                    link_uri='https://www.google.com/maps?q=中式料理',
+                    area=ImagemapArea(x=0, y=520, width=520, height=520)
+                ),
+                 URIImagemapAction(
+                    link_uri='https://www.google.com/maps?q=法式料理',
+                    area=ImagemapArea(x=520, y=520, width=520, height=520)
+                 )
+                # 可繼續加入其他類型料理
+            ]
+        )
+        line_bot_api.reply_message(event.reply_token, imagemap_message)
+
+    # 預設回應
     else:
-        line_bot_api.reply_message(reply_token, TextSendMessage(text=message))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=user_message))
 
 
-
-# if "新聞" in user_message:
-#         response = get_news()
-#     else:
-#         response = "請輸入「天氣」或「新聞」來獲取資訊。"
-#     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=response))
-
-#主程式
-import os
+# 主程式
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
